d41b3be04384ff3d3cda9dadc84dbcd5ebb4e1e2
cliswin <cliswin@gmail.com>
2019-04-15 18:22:59 +0800

    Update func_round.py

--- example/func_round.py
+++ example/func_round.py
@@ -1,5 +1,5 @@
 # 函数round()把浮点数四舍五入为最接近的整数值。
-# 注意：在python2.7版本中是如上所说，但是在python3.5版本中并不是这样的。
+# 注意：在python2.x版本中是如上所说，但是在python3.x版本中并不是这样的。
 
 var = round(1/2)
 print(var)
@@ -7,4 +7,4 @@ print(var)
 var = round(2.0/5.0)
 print(var)
 
-# 2.7版本：第一个var=0，第二个var=1.0； 3.5版本：第一个var=0.5，第二个var=0
+# 2.x版本：第一个var=0，第二个var=1.0； 3.x版本：第一个var=0.5，第二个var=0
