5724604b3dd34f66b53e4bd2c97819061760f194
cliswin <cliswin@gmail.com>
2019-04-28 14:47:01 +0800

    Create demo_elif.py

---
+++ example/demo_elif.py
@@ -0,0 +1,9 @@
+# elif 子句
+
+num = int(input('Enter a number: '))
+if num > 0:
+    print('the number is positive')
+elif num <0:
+    print('the number is negative')
+else:
+    print('the number is zero')
